264811a9a917809f7d763cfde32d893ee5bbba0b
Vrby <fv54764@fer.hr>
2026-04-23 12:09:28 +0200

    Add font bitmap

---
+++ include/font.h
@@ -0,0 +1,10 @@
+#ifndef FONT_H
+#define FONT_H
+
+#include <stdint.h>
+
+// A simple 8x8 bitmap font (subset of standard VGA font)
+// Each character is 8 bytes
+extern uint8_t font8x8_basic[128][8];
+
+#endif
